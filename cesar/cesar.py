3206497850a56a389a6dece3dfef50529195daec
JoaoPauloSousaCoelho <jpaulo.ufsj@gmail.com>
2023-02-21 16:08:52 -0300

    Testando Projeto

--- cesar/cesar.py
+++ cesar/cesar.py
@@ -1,31 +1,34 @@
 from string import ascii_lowercase
 
-def encripta(frase, n = 13):
-    """encripta o texto"""
+
+def encripta(frase, n=13):
+    """Encripta o texto."""
     encriptado = ''
-    for l in frase:
-        l = l.lower()
-        if l == ' ':
-            encriptado += l
-        elif l not in ascii_lowercase: ...
-            
+    for letra in frase:
+        letra = letra.lower()
+        if letra == ' ':
+            encriptado += letra
+        elif letra not in ascii_lowercase:
+            ...
+
         else:
-            pos = ascii_lowercase.find(l) + n
-            l = ascii_lowercase[pos % 26]
-            encriptado += l
+            pos = ascii_lowercase.find(letra) + n
+            letra = ascii_lowercase[pos % 26]
+            encriptado += letra
     return encriptado
 
 
-def decripta(frase, n = 13):
+def decripta(frase, n=13):
     """Decripta o texto"""
     decriptado = ''
-    for l in frase:
-        l = l.lower()
-        if l == ' ':
-            decriptado += l
-        elif l not in ascii_lowercase: ...
+    for letra in frase:
+        letra = letra.lower()
+        if letra == ' ':
+            decriptado += letra
+        elif letra not in ascii_lowercase:
+            ...
         else:
-            pos = ascii_lowercase.find(l) - n
-            l = ascii_lowercase[pos % 26]
-            decriptado += l
-    return decriptado                            
+            pos = ascii_lowercase.find(letra) - n
+            letra = ascii_lowercase[pos % 26]
+            decriptado += letra
+    return decriptado
