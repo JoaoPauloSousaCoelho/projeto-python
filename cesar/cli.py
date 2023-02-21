b70694c08dd180f09fb3abbdc2f5d155e538cd3e
Joao Paulo <jpaulo.ufsj@gmail.com>
2023-02-21 11:37:33 -0300

    initial version

---
+++ cesar/cli.py
@@ -0,0 +1,19 @@
+from argparse import ArgumentParser
+from cesar import encripta, decripta
+
+parser = ArgumentParser(description = 'Cifra de Cesar')
+parser.add_argument('frase', help = 'Frase a ser encriptada/decriptada', type = str)
+parser.add_argument('-n', help = 'Valor da rotação', default = 13, type = int, required = False)
+parser.add_argument('-d', help = 'Decripta', required = False, action = 'store_true')
+
+def cli():
+    args = parser.parse_args()
+    if args.d:
+        resultado = decripta(args.frase, args.n)
+    else:
+        resultado = encripta(args.frase, args.n)
+
+    print(f'Entrada: {args.frase}')
+    print(f'Saida: {resultado}')        
+
+cli()
